1351e342f8fdfccb32d9c82ddab68c06ad2075d2
Lancet52 <sazonov-vs@mail.ru>
2025-04-28 23:04:43 +0300

    комит

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
-
+import os
 
 class Trainer:
     def __init__(self, model, train_path, val_path, text_column='description', label_column='category'):
@@ -40,3 +40,34 @@ class Trainer:
         
         self.input_size = self.X_train.shape[1]
         self.num_classes = len(self.label_encoder.classes_)
+        
+    def create_train_folder(self):
+        """Создание папки для сохранения результатов обучения"""
+        base_dir = os.getcwd()
+        train_folders = []
+        
+        for folder in os.listdir(base_dir):
+            if os.path.isdir(os.path.join(base_dir, folder)) and folder.startswith("train_"):
+                train_folders.append(folder)
+        
+        folder_numbers = []
+        for f in train_folders:
+            try:
+                num = int(f.split("_")[-1])
+                folder_numbers.append(num)
+            except ValueError:
+                continue
+        
+        next_number = max(folder_numbers) + 1 if folder_numbers else 0
+        new_folder = f"train_{next_number}"
+        self.new_folder_path = os.path.join(base_dir, new_folder)
+        
+        os.makedirs(self.new_folder_path, exist_ok=True)
+        print(f"Создана папка: {self.new_folder_path}")
+        
+        # Создание файла для результатов
+        headers = ["epoch", "train_loss", "train_acc, %", 
+                  "val_loss", "val_acc, %", "F1_score"]
+        self.csv_path = f"{self.new_folder_path}/result.csv"
+        if not os.path.isfile(self.csv_path):
+            pd.DataFrame(columns=headers).to_csv(self.csv_path, index=False)
